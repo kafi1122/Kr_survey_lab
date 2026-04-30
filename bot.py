df3d8848a588b181a68c62ddb59b8bee7441af3e
kafi1122 <55059796+kafi1122@users.noreply.github.com>
2026-04-30 20:24:17 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 import os
 
-BOT_TOKEN = os.getenv("8770137480:AAF4R4lCzUZHD2VDMi_47K8iQINZ_TCiXIc")
+BOT_TOKEN = "8770137480:AAF4R4lCzUZHD2VDMi_47K8iQINZ_TCiXIc"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is running!")
