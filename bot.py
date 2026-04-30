824a9a63dbd9b77ed4849ed1ae983ecf445dc8b9
kafi1122 <55059796+kafi1122@users.noreply.github.com>
2026-04-30 20:58:34 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ BOT_TOKEN = ("8770137480:AAF4R4lCzUZHD2VDMi_47K8iQINZ_TCiXIc")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is running!")
 
-app = ApplicationBuilder().token(8770137480:AAF4R4lCzUZHD2VDMi_47K8iQINZ_TCiXIc).build()
+app = ApplicationBuilder().token("8770137480:AAF4R4lCzUZHD2VDMi_47K8iQINZ_TCiXIc").build()
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
